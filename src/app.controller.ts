76ed0cceef48c4cf2f8e47c867f3577f80301409
zaz7 <abdullah75farid@gmail.com>
2024-09-23 00:29:19 +0300

    database-final

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+
 import { DatabaseService } from './DatabaseModule/database.service';
 
 @Controller()
