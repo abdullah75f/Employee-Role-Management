9648bf32dde8e09d0721c458c9a7dde362e19999
zaz7 <abdullah75farid@gmail.com>
2024-09-22 22:05:22 +0300

    database

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable,Inject } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(@Inject('POSTGRES_POOL') private readonly sql:any){}
+  async getTable(name:string): Promise<any[]>{
+    return await this.sql(`SELECT * FROM ${name}`)
   }
 }
