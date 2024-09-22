6242e4a7b2497448cd899651f811c2718b7bce64
zaz7 <abdullah75farid@gmail.com>
2024-09-22 22:49:05 +0300

    database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,12 @@
 // src/app.module.ts
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+
 import { DatabaseModule } from './DatabaseModule/database.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true, // Makes the ConfigModule available globally
-    }),
     DatabaseModule,
   ],
   controllers: [AppController],
