8563dca1adf627776f990c6e63efedfae669801f
zaz7 <abdullah75farid@gmail.com>
2024-09-24 23:22:38 +0300

    database locaaly

--- src/DatabaseModule/database.controller.ts
+++ src/DatabaseModule/database.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { DatabaseService } from './database.service';
 
 @Controller('database')
 export class DatabaseController {
     constructor(private readonly databaseService: DatabaseService) {}
+
+    @Get('allroles')
+    async getroles(){
+        return this.databaseService.queryTable();
+    }
 }
