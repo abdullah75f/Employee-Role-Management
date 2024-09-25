da386001eb3f72f5848310f9abf1015384a47e68
zaz7 <abdullah75farid@gmail.com>
2024-09-25 23:24:00 +0300

    final commit

--- src/position/dto/create-position.dto.ts
+++ src/position/dto/create-position.dto.ts
@@ -1,7 +1,6 @@
-// create-position.dto.ts
-
+//class that defines the structure of data for creating a role
 export class CreateRoleDto {
   name: string;
   description: string;
-  parentId: string; // ID of the managing position/role
+  parentId: string; // ID of the managing role
 }
