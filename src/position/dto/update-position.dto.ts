95086a751e034ab97e9e8ceb3124fea9381d69fc
zaz7 <abdullah75farid@gmail.com>
2024-09-25 23:24:40 +0300

    final commit

--- src/position/dto/update-position.dto.ts
+++ src/position/dto/update-position.dto.ts
@@ -1,6 +1,4 @@
-// update-role.dto.ts
-
-import { IsString, IsNotEmpty, IsOptional } from 'class-validator';
+import { IsString, IsOptional } from 'class-validator';
 
 export class UpdateRoleDto {
   @IsOptional()
