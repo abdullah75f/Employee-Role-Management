0de02cda257193975fc4830744dcfd85123fdb16
zaz7 <abdullah75farid@gmail.com>
2024-09-25 23:17:09 +0300

    childre of a role

--- src/DatabaseModule/database.controller.ts
+++ src/DatabaseModule/database.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { DatabaseService } from './database.service';
 
+//Routes 
 @Controller('database')
 export class DatabaseController {
     constructor(private readonly databaseService: DatabaseService) {}
