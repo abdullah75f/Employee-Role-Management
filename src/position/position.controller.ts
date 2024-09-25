5295d46b875135254146e8d925c473d418979dbf
zaz7 <abdullah75farid@gmail.com>
2024-09-25 23:44:23 +0300

    final commit

--- src/position/position.controller.ts
+++ src/position/position.controller.ts
@@ -1,4 +1,3 @@
-
 import { Controller, Post, Body,Put, Param,Get,Delete} from '@nestjs/common';
 import { RoleService } from './position.service';
 import { CreateRoleDto } from './dto/create-position.dto';
@@ -8,32 +7,43 @@ import { Role } from './entities/position.entity';
 @Controller('positions')
 export class PositionController {
   constructor(private readonly positionService: RoleService) {}
+
+  //Create
   //fixed
   @Post()
   async createPosition(@Body() createRoleDto: CreateRoleDto): Promise<Role> {
     return this.positionService.createPosition(createRoleDto);
   }
+
+  //Update
   //fixed
   @Put(':id')
   async updateRole(@Param('id') id: string, @Body() updateRoleDto: UpdateRoleDto): Promise<Role> {
     return this.positionService.updateRole(id, updateRoleDto);
   }
+
+  //Get
   //fixed
   @Get(':id')
   async getPositionById(@Param('id') id: string): Promise<Role | Role[]> {
     return this.positionService.getPositionById(id);
   }
-  //not working
+  //Get
+  //fixed
   @Get('structure')
   async getPositionHierarchy(): Promise<Role[]> {
     return this.positionService.getPositionHierarchy();
   }
+
+    //Get
   //fixed
   @Get(':id/children')
   async getChildrenOfPosition(@Param('id') id: string): Promise<Role[]> {
     return this.positionService.getChildrenOfPosition(id);
   }
 
+  //Delete
+  //fixed
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.positionService.removeRole(id);
