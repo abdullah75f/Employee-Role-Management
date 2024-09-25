c4cff087b5d7ba91489e0b1ec67b69dd147b8553
zaz7 <abdullah75farid@gmail.com>
2024-09-25 23:21:29 +0300

    final commit

--- src/DatabaseModule/database.service.ts
+++ src/DatabaseModule/database.service.ts
@@ -1,23 +1,23 @@
-// src/database/database.service.ts
+
 import { Injectable, Inject } from '@nestjs/common';
 import {Client} from 'pg'
 
 @Injectable()
 export class DatabaseService {
+  //instantiate my class overhere
   constructor(@Inject('POSTGRES_CLIENT') private readonly client:Client) {}
-
+    //my business logic for querying my database
   async queryTable() : Promise<any[]>{
-
     const result =  await this.client.query(`SELECT * FROM role;`);
     return result.rows;
   
   }
   
 
-  async checkConnection(): Promise<void> {
-    await this.client.query('SELECT 1');
-    console.log('Database connection is healthy');
-  }
+  // async checkConnection(): Promise<void> {
+  //   await this.client.query('SELECT 1');
+  //   console.log('Database connection is healthy');
+  // }
 }
 
 
